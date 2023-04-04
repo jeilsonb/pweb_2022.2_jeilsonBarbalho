0ecab8650b7017e0268471b406a9bbfcd8fd038f
Jeilsonb <jeilsong230@gmail.com>
2023-04-04 20:12:54 -0300

    Alterações na Interface e Mudanças no ClienteController

--- biritashop/src/main/java/br/com/jeilson/biritashop/model/Cliente.java
+++ biritashop/src/main/java/br/com/jeilson/biritashop/model/Cliente.java
@@ -31,6 +31,4 @@ public class Cliente implements Serializable {
     private String CEP;
     @OneToMany(mappedBy = "cliente")
     private List<Dependente> dependentes;
-    @OneToMany(mappedBy = "cliente")
-    private List<Pedido> pedidos;
 }
--- biritashop/src/main/java/br/com/jeilson/biritashop/repository/ItemPedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.jeilson.biritashop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.jeilson.biritashop.model.ItemPedido;
-
-public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
-    
-}
